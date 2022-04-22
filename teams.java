2855ffc4c7353521c4b58011078c05adaacf0dff
RachelGoodrich <71162103+RachelGoodrich@users.noreply.github.com>
2022-04-22 15:38:45 -0700

    Update teams.java

--- teams.java
+++ teams.java
@@ -4,6 +4,9 @@ import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  *  Recreation of Teams with design updates
@@ -21,33 +24,104 @@ public class teams extends Application
    public void start(Stage primaryStage)
    {
       // Create a Label component.
+      
+      
+      Image activitypic = new Image("File:activitybuttonpic.PNG");
+      ImageView activityview = new ImageView(activitypic);
+      activityview.setFitHeight(50);
+      activityview.setPreserveRatio(true);
+
+      Image chatpic = new Image("File:chatbuttonpic.PNG");
+      ImageView chatview = new ImageView(chatpic);
+      chatview.setFitHeight(50);
+      chatview.setPreserveRatio(true);
+
+      Image teampic = new Image("File:teamsbuttonpic.PNG");
+      ImageView teamview = new ImageView(teampic);
+      teamview.setFitHeight(50);
+      teamview.setPreserveRatio(true);
+
+      Image assignpic = new Image("File:assignbuttonpic.PNG");
+      ImageView assignview = new ImageView(assignpic);
+      assignview.setFitHeight(50);
+      assignview.setPreserveRatio(true);
+
+      Image calendarpic = new Image("File:calendarbuttonpic.PNG");
+      ImageView calendarview = new ImageView(calendarpic);
+      calendarview.setFitHeight(50);
+      calendarview.setPreserveRatio(true);
+
+      Image callspic = new Image("File:callsbuttonpic.PNG");
+      ImageView callsview = new ImageView(callspic);
+      callsview.setFitHeight(50);
+      callsview.setPreserveRatio(true);
+
+      Image filespic = new Image("File:filesbuttonpic.PNG");
+      ImageView filesview = new ImageView(filespic);
+      filesview.setFitHeight(50);
+      filesview.setPreserveRatio(true);
+
+      Image morepic = new Image("File:morebuttonpic.PNG");
+      ImageView moreview = new ImageView(morepic);
+      moreview.setFitHeight(50);
+      moreview.setPreserveRatio(true);
+
+
+      
       Label searchLabel = new Label("search");
-      Label activity = new Label("Activity");
-      Label chat = new Label("Chat");
-      Label teams = new Label("Teams");
-      Label sheCodes = new Label("sheCodes");
-      Label groupChat = new Label("Group Chat");
-      Label voiceChannel = new Label("voiceChannel");
-      Label post1 = new Label("Post 1");
-      Label post2 = new Label("Post 2");
+
+      Button activity = new Button();
+      activity.setGraphic(activityview);
+      activity.setPrefSize(50, 50);
+
+      Button chat = new Button();
+      chat.setGraphic(chatview);
+      chat.setPrefSize(50, 50);
+
+      Button teams = new Button();
+      teams.setGraphic(teamview);
+      teams.setPrefSize(50, 50);
+
+        
+      Button assignments = new Button();
+      assignments.setGraphic(assignview);
+      assignments.setPrefSize(50, 50);
+
+      Button calendar = new Button();
+      calendar.setGraphic(calendarview);
+      calendar.setPrefSize(50, 50);
+
+      Button calls = new Button();
+      calls.setGraphic(callsview);
+      calls.setPrefSize(50, 50);
+
+      Button files = new Button();
+      files.setGraphic(filesview);
+      files.setPrefSize(50, 50);
+
+      Button more = new Button();
+      more.setGraphic(moreview);
+      more.setPrefSize(50, 50);
+
       Label members = new Label("members");
       Label rachel = new Label("members");
+      Button shecodesbutton = new Button("sheCodes");
       
       // Create other boxes
       HBox searchBar = new HBox(searchLabel);
-      VBox left = new VBox(activity, chat, teams);
-      VBox channels = new VBox(sheCodes, groupChat, voiceChannel);
-      VBox bulletin = new VBox(post1, post2);
-      VBox groupchat = new VBox(members);
+      VBox left = new VBox(activity, chat, teams, assignments,calendar,calls,files,more);
+     
+      
+     
       VBox membersList = new VBox(members, rachel);
+      HBox main = new HBox(shecodesbutton);
       left.getStyleClass().add("left-side");
-      channels.getStyleClass().add("channels");
-      bulletin.getStyleClass().add("bulletin");
+     
       membersList.getStyleClass().add("members-list");
       searchBar.getStyleClass().add("search-bar");
       
       // Create a Scene with the VBox as its root node.
-      HBox hbox = new HBox(10, left, channels, bulletin, membersList);
+      HBox hbox = new HBox(10, left,  membersList,main);
       VBox vbox = new VBox(searchBar, hbox);
 
       Scene scene = new Scene(vbox);
